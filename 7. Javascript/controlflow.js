c21bd4d2e32da518c46952b916129f8f5c6fda6b
Afabozz <afaboss44@gmail.com>
2025-05-21 20:55:29 +0700

    javascript

---
+++ 7. Javascript/controlflow.js
@@ -0,0 +1,11 @@
+// alert('Mulai');
+// for(var i = 0; i < 5; i++) {
+//     alert('Hello World!');
+// }
+// alert('Selesai');
+
+var ulang = true;
+while (ulang) {
+  console.log('Hello World!');
+  ulang = confirm('Lagi?');
+}
