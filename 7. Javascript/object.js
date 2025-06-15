72783e802aab3808b1916bef2c568210873acef1
Afabozz <afaboss44@gmail.com>
2025-06-16 06:45:27 +0700

    16/06/25

---
+++ 7. Javascript/object.js
@@ -0,0 +1,11 @@
+// membuat Object
+var mhs = {
+    nama : "Abdullah Kafabihi",
+    umur : 21,
+    ips : [3.00, 2.50, 3.20],
+    alamat : {
+        jalan : "Jl. Jatipelem No.00",
+        kota : "Jombang",
+        provinsi : "Jawa Timur"
+    }
+};
